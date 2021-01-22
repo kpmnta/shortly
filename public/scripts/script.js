2de1d951fccef39bf18f203f4fbd5d3863d5713f
kai <kai.pimenta1@gmail.com>
2021-01-22 15:12:34 -0300

    refeito script hamburguer navbar

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,10 +1,19 @@
 function initMenuMobile() {
-    const navbar = document.getElementById("navbar");
-    if (navbar.className === "menu") {
-        navbar.className += " responsive";
-        console.log('abriu');
+    const links = document.getElementById("hidden");
+    if (links.style.display === 'none') {
+      links.style.display = 'block';
     } else {
-      navbar.className = "menu";
-      console.log('fechou');
+      links.style.display = 'none';
     }
 }
+
+// function initMenuMobile() {
+//   const navbar = document.getElementById("navbar");
+//   if (navbar.className === "menu") {
+//       navbar.className += " responsive";
+//       console.log('abriu');
+//   } else {
+//     navbar.className = "menu";
+//     console.log('fechou');
+//   }
+// }
